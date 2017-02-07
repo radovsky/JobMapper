16ab4d48a9504a43ad9aaef8603c2bb75938df1c
Gabe <gabriel.radovsky@gmail.com>
2017-02-06 23:28:58 -0700

    model accessor/validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  attr_accessor :password
+
+  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[0-9]).{6,62}$/i
+
+  validates :username, presence: true, uniqueness: true, length: { in: 2..20 }
+  validates :email, presence: true, uniqueness: true, format: EMAIL_REGEX
+  validates :password, confirmation: true, format: PASSWORD_REGEX
 end
